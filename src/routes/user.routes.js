ea650a3f4beb427092af1ee4d4dafa55f9cd1c2d
govindkumar20 <govindp231@gmail.com>
2026-01-04 20:02:31 +0530

    name fixing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ import {
     getUserChannelProfile, 
     getWatchHistory, 
     updateAccountDetails
-} from "../controllers/user.controller.js";
+} from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
