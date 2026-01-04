8aceaaa062864d6bbedee78ad73b97d9e390d7b9
govindkumar20 <govindp231@gmail.com>
2026-01-04 22:18:24 +0530

    CHANG

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(express.json({limit: "16mb"}))
 app.use(express.urlencoded({extended: true, limit: "16mb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(multerErrorHandler)
 
 
 //routes import
